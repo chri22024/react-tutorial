9ed63b76caee6f0c15735230db3b42ccfe6f6a06
rinnnn55 <125744070+rinnnn55@users.noreply.github.com>
2023-10-20 15:43:56 +0900

    react-tutorial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./Main";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
